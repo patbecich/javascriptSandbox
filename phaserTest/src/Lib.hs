94da5420778f05213c563dd32a1190573b472572
Patrick <patbecich@gmail.com>
2017-04-19 14:54:56 -0700

    POST requests for breakOut game

--- phaserTest/src/Lib.hs
+++ phaserTest/src/Lib.hs
@@ -2,12 +2,28 @@
 
 module Lib ( scottyMain ) where
 
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Class
 import           Data.Monoid                   (mconcat)
 import           Data.Text.Lazy                (pack)
 import           Network.Wai.Middleware.Static
 import           Web.Scotty
 
+
 scottyMain :: IO ()
-scottyMain = scotty 3000 $ do
-    middleware $ staticPolicy (noDots >-> addBase "static")
-    get "/" $ file "static/deflection.html"
+scottyMain = scotty 8000 $ do
+  middleware $ staticPolicy (noDots >-> addBase "static")
+  get "/" $ file "static/deflection.html"
+  get "/breakOut" $ file "static/breakOut.html"
+  get "/scotty/:word" $ do
+    beam <- param "word"
+    html (mconcat
+      [ "<h1>Scotty, "
+      , beam
+      , " me up!</h1>"])
+  post "/breakOutScore" $ do
+    score <- param "score" :: ActionM Int
+    _ <- liftIO $ putStrLn $ "score received: " ++ show score
+    text "score received"
+  get "/foo" $ do
+    html $ "hello from Scotty"
