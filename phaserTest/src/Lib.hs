aa1e798f33f01ce0c4edc1da56f96ea0382f99cf
Patrick <patbecich@gmail.com>
2017-04-19 19:42:29 -0700

    phaser first game tutorial

--- phaserTest/src/Lib.hs
+++ phaserTest/src/Lib.hs
@@ -15,6 +15,8 @@ scottyMain = scotty 8000 $ do
   middleware $ staticPolicy (noDots >-> addBase "static")
   get "/" $ file "static/deflection.html"
   get "/breakOut" $ file "static/breakOut.html"
+  get "/helloPhaser" $ file "static/helloPhaser.html"
+  get "/phaserMario" $ file "static/phaserMario.html"
   get "/scotty/:word" $ do
     beam <- param "word"
     html (mconcat
@@ -27,3 +29,22 @@ scottyMain = scotty 8000 $ do
     text "score received"
   get "/foo" $ do
     html $ "hello from Scotty"
+  get "/phaser/gettingStarted/" $ file "static/phaser/gettingStarted/index.html"
+  get "/phaser/gettingStarted/part2" $ file "static/phaser/gettingStarted/part2.html"
+  get "/phaser/gettingStarted/part3" $ file "static/phaser/gettingStarted/part3.html"
+  get "/phaser/gettingStarted/part4" $ file "static/phaser/gettingStarted/part4.html"
+  get "/phaser/gettingStarted/part5" $ file "static/phaser/gettingStarted/part5.html"
+  get "/phaser/gettingStarted/part6" $ file "static/phaser/gettingStarted/part6.html"
+  get "/phaser/gettingStarted/part7" $ file "static/phaser/gettingStarted/part7.html"
+  get "/phaser/gettingStarted/part8" $ file "static/phaser/gettingStarted/part8.html"
+  get "/phaser/firstGame/" $ file "static/phaser/firstGame/tutorial.html"
+  get "/phaser/firstGame/part1" $ file "static/phaser/firstGame/part1.html"
+  get "/phaser/firstGame/part2" $ file "static/phaser/firstGame/part2.html"
+  get "/phaser/firstGame/part3" $ file "static/phaser/firstGame/part3.html"
+  get "/phaser/firstGame/part4" $ file "static/phaser/firstGame/part4.html"
+  get "/phaser/firstGame/part5" $ file "static/phaser/firstGame/part5.html"
+  get "/phaser/firstGame/part6" $ file "static/phaser/firstGame/part6.html"
+  get "/phaser/firstGame/part7" $ file "static/phaser/firstGame/part7.html"
+  get "/phaser/firstGame/part8" $ file "static/phaser/firstGame/part8.html"
+  get "/phaser/firstGame/part9" $ file "static/phaser/firstGame/part9.html"
+
