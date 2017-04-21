f1fc6615c00910296d879a5be8ec9f3deb6635d7
Peter Becich <peterbecich@gmail.com>
2017-04-20 22:57:36 -0700

    yesod book

---
+++ yesodBook/src/Lib.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+
+module Lib
+    ( main'
+    ) where
+
+import           Yesod
+
+-- http://www.yesodweb.com/book/basics
+
+data HelloWorld = HelloWorld
+
+mkYesod "HelloWorld" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod HelloWorld
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|Hello World!|]
+
+main' :: IO ()
+main' = warp 3000 HelloWorld
+
