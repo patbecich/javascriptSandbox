34131f77d03babe40c014ba5776e6968f72c5dbf
Patrick <patbecich@gmail.com>
2017-04-22 22:00:10 -0700

    changed port to 80

--- fish/app/Main.hs
+++ fish/app/Main.hs
@@ -18,7 +18,7 @@ main :: IO ()
 main =
     do ref <- newIORef 0
        spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
-       runSpock 8080 (spock spockCfg app)
+       runSpock 80 (spock spockCfg app)
 
 app :: SpockM () MySession MyAppState ()
 app =
